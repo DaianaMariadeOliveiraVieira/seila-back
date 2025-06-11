e947d4c01538480f09937bcfcb397190b30a4e42
DaianaMariadeOliveiraVieira <daiana_vieira99@hotmail.com>
2025-06-11 09:57:27 -0300

    primeiro

---
+++ ds-projeto-seila/src/main/java/com/cefet/ds_projeto_seila/DsProjetoSeilaApplication.java
@@ -0,0 +1,13 @@
+package com.cefet.ds_projeto_seila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsProjetoSeilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsProjetoSeilaApplication.class, args);
+	}
+
+}
---
+++ ds-projeto-seila/src/test/java/com/cefet/ds_projeto_seila/DsProjetoSeilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefet.ds_projeto_seila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsProjetoSeilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
