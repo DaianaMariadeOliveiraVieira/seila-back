58d6b7d30e0a80bd0004c727678dad6529bce57d
DaianaMariadeOliveiraVieira <daiana_vieira99@hotmail.com>
2025-06-24 15:48:54 -0300

    Implementar entidade Cliente com anotações JPA e campos básicos

--- ds-projeto-seila/src/main/java/com/cefet/ds_projeto_seila/entities/Cliente.java
+++ ds-projeto-seila/src/main/java/com/cefet/ds_projeto_seila/entities/Cliente.java
@@ -1,5 +1,82 @@
 package com.cefet.ds_projeto_seila.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbCliente")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String nome;
+    private String cpf;
+    private String email;
+    private String login;
+    private String senha;
+
+    
+    public Cliente() {
+    }
+
+    
+    public Cliente(Long id, String nome, String cpf, String email, String login, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
+
