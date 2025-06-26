c00d6a2c87912b79b7750f04104d0b088787351c
DaianaMariadeOliveiraVieira <daiana_vieira99@hotmail.com>
2025-06-26 15:54:30 -0300

    update e delete no PlanoService

--- ds-projeto-seila/src/main/java/com/cefet/ds_projeto_seila/services/PlanoService.java
+++ ds-projeto-seila/src/main/java/com/cefet/ds_projeto_seila/services/PlanoService.java
@@ -38,4 +38,29 @@ public class PlanoService {
         return new PlanoDTO(saved);
     }
 
+    public PlanoDTO update(Long id, PlanoDTO dto) {
+        Plano plano = planoRepo.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Plano não encontrado com ID: " + id));
+
+        if (dto.getValor() <= 0)
+            throw new IllegalArgumentException("O valor do plano deve ser um número positivo");
+
+        // verificar se a descrição já existe para outro plano ....
+        if (planoRepo.existsByDescricao(dto.getDescricao()) && !plano.getDescricao().equals(dto.getDescricao())) {
+            throw new IllegalArgumentException("Já existe um plano com a descrição: " + dto.getDescricao());
+        }
+
+        plano.setDescricao(dto.getDescricao());
+        plano.setValor(dto.getValor());
+
+        Plano updated = planoRepo.save(plano);
+        return new PlanoDTO(updated);
+    }
+
+    public void delete(Long id) {
+        if (!planoRepo.existsById(id)) {
+            throw new EntityNotFoundException("Plano não encontrado com ID: " + id);
+        }
+        planoRepo.deleteById(id);
+    }
 }
