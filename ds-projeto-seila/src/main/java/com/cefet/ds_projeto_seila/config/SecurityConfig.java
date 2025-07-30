fbd2bd17bed40fc895f6c0b0319ee60be7d28f4f
Sofia de Freitas <freitasmsofia@gmail.com>
2025-07-30 08:59:36 -0300

    link do netlify em allowed origins

--- ds-projeto-seila/src/main/java/com/cefet/ds_projeto_seila/config/SecurityConfig.java
+++ ds-projeto-seila/src/main/java/com/cefet/ds_projeto_seila/config/SecurityConfig.java
@@ -119,7 +119,7 @@ public class SecurityConfig {
           @Override
           public void addCorsMappings(CorsRegistry registry) {
               registry.addMapping("/**")
-                  .allowedOrigins("http://localhost:4200")
+                  .allowedOrigins("http://localhost:4200", "https://seila-front.netlify.app/")
                   .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                   .allowedHeaders("*")
                   .allowCredentials(true);
